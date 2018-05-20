7e34b57c51a1c50a836de64a1de856cb242b9649
YuriyDobryanskiy <dobryansky@i.ua>
2018-05-21 00:33:17 +0300

    swipe
    new swipe property

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
 $(function () {
-    $('body').on('swipeup',swipeHandler);
-    $('body').on('swipedown',swipeHandler2);
+    $('#wifi').on('swipeup',swipeHandler);
+    $('#wifi').on('swipedown',swipeHandler2);
 
     function swipeHandler( event ){
-        $('body').removeClass("swipeDown");
-        $('body').addClass( "swipeUp" );
+		$('#kava').css("filter", "blur(3px)");
+		$('#wifi').css({"bottom":"0px", "transition": "1s"});
+		$('#arrow').css({"background-image":"url('img/4_1.png')","height":"22px","margin":"-6% auto 0","animation":"none","transition":"1s"});
     };
     function swipeHandler2( event ){
-        $('body').addClass( "swipeDown" );
-        $('body').removeClass("swipeUp");
+		$('#kava').css("filter", "blur(0px)");
+		$('#wifi').css({"bottom":"-130px", "transition": "1s"});
+		$('#arrow').css({"background-image":"url('img/4.png')","height":"62px","margin":"-66px auto 0","animation":"blinker 2s linear infinite","transition":"1s"});
     };
 })
